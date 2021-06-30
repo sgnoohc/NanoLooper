1c61ee8f5e151a39c6c134caebcaa4f83a7328ed
Yuntong Zhou <joytzphysics@uaf-8.t2.ucsd.edu>
2021-06-29 20:28:41 -0700

    Changes to be committed: 	modified:   NanoLooper/main.cc NanoLooper/main.o NanoLooper/plot.py 	NanoLooper/plots/ NanoLooper/wzh.root 	runNanoLooper 	vbswwh.txt 	vbswzh.txt

--- NanoLooper/main.cc
+++ NanoLooper/main.cc
@@ -2,66 +2,91 @@
 #include "Nano.h" // Contains the definition of the "nt" object that reads the NanoAOD
 #include "MCTools.h" // Contains the definition of the dumpGenParticleInfos();
 #include "rooutil.h" // Contains the definitions of various help functions that start with "RooUtil"
-
 int main()
 {
+    // Create your output root file
+    TFile* output_file = new TFile("/home/users/joytzphysics/NanoLooper/NanoLooper/wzh.root", "recreate");
+    // pt
+    TH1F* ptZ = new TH1F("ptZ", "Transverse momentum of Z bosons", 1080, 0, 1800);
+    TH1F* ptW = new TH1F("ptW", "Transverse momentum of W bosons", 1080, 0, 1800);
+    TH1F* ptH = new TH1F("ptH", "Transverse momentum of H bosons", 1080, 0, 1800);
+    // VBF quarks
+    TH1F* deltaEta = new TH1F("deltaEta", "Delta Eta of VBF quarks", 1080, 0, 10);
+    TH1F* massVBF = new TH1F("massVBF", "Invariant mass of VBF quark system", 1080, 0, 3500);
+    // Cross section
+    Double_t cs = 0.018 * 137;
 
-    // Input file path (comma separated string).
-    TString input_file_list_tstring = "/hadoop/cms/store/user/phchang/VBSHWWSignalGeneration/VBSWWH_C2V_4p5_RunIIAutumn18NanoAOD_VBSWWH_C2V_4p5_v3_ext1/merged/output.root";
+    // Input Path
+    for (unsigned i = 0; i < 26; i++) 
+    {
+	TString input = "/hadoop/cms/store/user/phchang/VBSHWWSignalGeneration/UL18_VBSWZH_incl_C2V_4_Azure_v1/merged/output_" + std::to_string(i) + ".root";
 
     // Name of the TTree in the input root files.
-    TString input_tree_name = "Events";
+	TString input_tree_name = "Events";
 
     // Create the TChain that holds the TTree's of the NanoAOD ntuples.
-    TChain* events_tchain = RooUtil::FileUtil::createTChain(input_tree_name, input_file_list_tstring);
+	TChain* events_tchain = RooUtil::FileUtil::createTChain(input_tree_name, input);
 
     // Max number of events to loop. (-1 = loop over all)
-    int n_events = 1; // Looping over first event only
-    // int n_events = -1; // To loop over all events
+    // int n_events = 1; // Looping over first event only
+	int n_events = -1; // To loop over all events
 
     // Create a looper that will handle event looping
-    RooUtil::Looper<Nano> looper;
+	RooUtil::Looper<Nano> looper;
 
+	nt.SetYear(2018);
     // Initializer the looper
-    looper.init(events_tchain, &nt, n_events);
+	looper.init(events_tchain, &nt, n_events);
 
-    // Create your output root file
-    // TFile* output_file = new TFile("output.root", "recreate");
-
-    // Create your histograms
-    // TH1F* ...
-    // TH1F* ...
-    // TH1F* ...
-    // TH1F* ...
-
-    while (looper.nextEvent())
-    {
+    
+        // Loop through events
+	while (looper.nextEvent())
+	{
         // Do your analysis here
         // float mjj = blah blah;
 
-        // Accessing the generator level information through branches with names that start with "GenPart_"
-
-        // To access the TTree content we use a "nt" object which is already taking care of various "SetBranchAddress" behind the scenes.
-        // By simply calling nt instance's methods with the same name as the TTree branch names, one can access the content of the TTree.
-        for (int ipart = 0; ipart < nt.GenPart_pt().size(); ++ipart)
-        {
-            std::cout << "ipart = " << ipart << " Transverse momentum (pt)    = " << nt.GenPart_pt()[ipart] << std::endl;
-            std::cout << "ipart = " << ipart << " Pseudorapditiy      (eta)   = " << nt.GenPart_eta()[ipart] << std::endl;
-            std::cout << "ipart = " << ipart << " Azimuthal angle     (phi)   = " << nt.GenPart_phi()[ipart] << std::endl;
-            std::cout << "ipart = " << ipart << " PDGID               (pdgid) = " << nt.GenPart_pdgId()[ipart] << std::endl;
-        }
-
+	// Select Leptons for the event
+	
+	    bool isvbswzh = nt.GenPart_status()[2] == 23;
+	    
+	    if (isvbswzh)
+	    {
+	    // LV ordered [NObjects];
+		const LV& ijet = nt.GenPart_p4()[2];
+		const LV& jjet = nt.GenPart_p4()[3];
+		const LV& jet0 = ijet.pt() > jjet.pt() ? ijet : jjet;
+		const LV& jet1 = ijet.pt() > jjet.pt() ? jjet : ijet;
+		const LV& W = nt.GenPart_p4()[4];
+		const LV& Z = nt.GenPart_p4()[5];
+		const LV& H = nt.GenPart_p4()[6];
+	    // Fill histograms
+		    massVBF->Fill((ijet + jjet).M());
+		    deltaEta->Fill(TMath::Abs(ijet.Eta()-jjet.Eta()));		
+		    ptZ->Fill(Z.Pt());
+		    ptW->Fill(W.Pt());
+		    ptH->Fill(H.Pt());	
+	    }
+	}
         // Alternatively, one can print the same (and more) information using tools developed by our research group's (and colleagues).
-        dumpGenParticleInfos();
+        // dumpGenParticleInfos();
 
     }
 
     // Done with the analysis
-    // output_file->cd();
-    // hist->Write();
-    // ...
-    // ...
-    // ...
+    output_file->cd();
+    massVBF->Scale(cs / (massVBF->Integral()));
+    massVBF->Write();
+    deltaEta->Scale(cs / (deltaEta->Integral()));
+    deltaEta->Write();
+    ptH->Scale(cs / (ptH->Integral()));
+    ptH->Write();
+    ptW->Scale(cs / (ptW->Integral()));
+    ptW->Write();
+    ptZ->Scale(cs / (ptZ->Integral()));
+    ptZ->Write();
 
+    output_file->Close(); 
+
+    
     return 0;
 }
